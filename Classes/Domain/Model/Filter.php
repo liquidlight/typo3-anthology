4ba63512998d66b32540d03c109939caf64103c8
Chris Tebbit <chris@liquidlight.co.uk>
2025-10-02 16:46:30 +0100

    feat: Output filters in list view and retain values

--- Classes/Domain/Model/Filter.php
+++ Classes/Domain/Model/Filter.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace LiquidLight\Anthology\Domain\Model;
 
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Domain\Repository\PageRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Domain\Model\Category;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
@@ -17,9 +21,71 @@ class Filter extends AbstractEntity
 
 	public array $searchFields = [];
 
+	public string $placeholder = '';
+
 	public string $dateField = '';
 
 	public string $dateSpan = '';
 
 	public ?Category $category = null;
+
+	protected mixed $value = null;
+
+	public function getCategories(): array
+	{
+		$connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+		$pageRepository = GeneralUtility::makeInstance(PageRepository::class);
+
+		$queryBuilder = $connectionPool->getQueryBuilderForTable('sys_category');
+
+		$queryBuilder
+			->select('uid')
+			->from('sys_category')
+			->where(
+				$queryBuilder->expr()->eq(
+					'parent',
+					$queryBuilder->createNamedParameter(
+						$this->category->getUid(),
+						Connection::PARAM_INT
+					)
+				)
+			)
+			->orderBy('sorting')
+		;
+
+		$childCategoryUids = array_column(
+			$queryBuilder->executeQuery()->fetchAllAssociative(),
+			'uid'
+		);
+
+		return array_map(
+			fn ($childCategoryUid) => $pageRepository
+				->getLanguageOverlay(
+					'sys_category',
+					$pageRepository->getRawRecord(
+						'sys_category',
+						$childCategoryUid
+					)
+				),
+			$childCategoryUids
+		);
+	}
+
+	public function getValue(): mixed
+	{
+		return $this->value;
+	}
+
+	public function setValue(mixed $value): void
+	{
+		switch ($this->filterType) {
+			case 'category':
+				$this->value = (int)$value;
+				break;
+
+			default:
+				$this->value = $value;
+				break;
+		}
+	}
 }
