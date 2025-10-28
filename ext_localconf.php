acc7fb8afef06f8b5f15a9e90d145c08115fea10
Chris Tebbit <chris@liquidlight.co.uk>
2025-10-28 11:27:21 +0000

    feat: Add Anthology view plugin

---
+++ ext_localconf.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use LiquidLight\Anthology\Controller\AnthologyController;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+defined('TYPO3') or die();
+
+call_user_func(function () {
+	ExtensionUtility::configurePlugin(
+		'LlAnthology',
+		'AnthologyView',
+		[
+			AnthologyController::class => 'view,list,single',
+		],
+		[
+			AnthologyController::class => 'view,list,single',
+		]
+	);
+});
