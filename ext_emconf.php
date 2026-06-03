3232dc5bc536a991a02f36c92c164e26302ef966
Matt Sheehan <matt.sheehan@liquidlight.co.uk>
2026-06-03 16:13:39 +0100

    feat: Add version 14 to requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.0.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '13.4.0-13.4.99',
+			'typo3' => '13.4.0-14.3.99',
 			'php' => '8.2.0-8.4.99',
 		],
 		'conflicts' => [
