66d5915e8e79178cb9035a54df1df1065f33fb03
Chris Tebbit <chris@liquidlight.co.uk>
2026-06-17 14:42:57 +0100

    feat: Add `Content` model and repository

---
+++ Classes/Domain/Model/Content.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiquidLight\Anthology\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+class Content extends AbstractEntity
+{
+}
--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 use LiquidLight\Anthology\Domain\Model\Category;
+use LiquidLight\Anthology\Domain\Model\Content;
 
 return [
 	Category::class => [
 		'tableName' => 'sys_category',
+	],
+	Content::class => [
+		'tableName' => 'tt_content',
 	],
 ];
