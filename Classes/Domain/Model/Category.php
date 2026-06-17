f5dd8625b9993a08131bf9fc2def49f42421ad2d
Chris Tebbit <chris@liquidlight.co.uk>
2026-06-17 14:42:21 +0100

    feat: Add `Category` model and repository

---
+++ Classes/Domain/Model/Category.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiquidLight\Anthology\Domain\Model;
+
+use TYPO3\CMS\Extbase\Domain\Model\Category as ExtbaseCategory;
+
+class Category extends ExtbaseCategory
+{
+}
---
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LiquidLight\Anthology\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class CategoryRepository extends Repository
+{
+}
